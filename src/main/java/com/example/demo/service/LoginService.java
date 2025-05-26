daf481bb7c62a13b42fbd4a855dd35931bba8606
krushna26 <Pawar@2601>
2025-05-26 08:48:10 +0530

    Added Changes for the entity and added repository implementationand added service and controller layer

---
+++ src/main/java/com/example/demo/service/LoginService.java
@@ -0,0 +1,8 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.Login;
+
+public interface LoginService {
+	Login userLogin(Login login);
+
+}
---
+++ src/main/java/com/example/demo/service/registerServiceImpl.java
@@ -0,0 +1,14 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.Register;
+
+public class registerServiceImpl implements registerService {
+
+	@Override
+	public Register userRegister(Register register) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+}
